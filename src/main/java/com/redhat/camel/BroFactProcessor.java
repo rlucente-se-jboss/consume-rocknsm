25b053e57f1422258ed447d54a907c02d48cfa06
rlucente <rlucente@redhat.com>
2018-09-26 11:02:00 -0400

    remove redundant code

--- src/main/java/com/redhat/camel/BroFactProcessor.java
+++ src/main/java/com/redhat/camel/BroFactProcessor.java
@@ -1,33 +1,11 @@
 package com.redhat.camel;
 
-import java.time.Instant;
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.Processor;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import it.redhat.hacep.playground.rules.model.BroFact;
 
-public class BroFactProcessor implements Processor {
-	private static final ObjectMapper mapper = new ObjectMapper();
-	private static final AtomicLong uniqueId = new AtomicLong();
+public class BroFactProcessor extends AbstractRockNSMProcessor {
 
 	public void process(Exchange exchange) throws Exception {
-		Message inMessage = exchange.getIn();
-		String jsonString = inMessage.getBody(String.class);
-		JsonNode objTree = mapper.readTree(jsonString);
-
-		long id = uniqueId.incrementAndGet();
-		String system = objTree.at("/@system").asText();
-		Date timestamp = Date.from(Instant.parse(objTree.at("/ts").asText()));
-
-		inMessage.setHeader("JMSXGroupID", system);
-		inMessage.setHeader("JMSXGroupSeq", id);
-		inMessage.setBody(new BroFact(id, jsonString, system, timestamp));
+		doProcess(exchange, "/@system", "/ts", BroFact.class);
 	}
 }
--- src/main/java/com/redhat/camel/SuricataFactProcessor.java
+++ src/main/java/com/redhat/camel/SuricataFactProcessor.java
@@ -1,33 +1,11 @@
 package com.redhat.camel;
 
-import java.time.Instant;
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.Processor;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import it.redhat.hacep.playground.rules.model.SuricataFact;
 
-public class SuricataFactProcessor implements Processor {
-	private static final ObjectMapper mapper = new ObjectMapper();
-	private static final AtomicLong uniqueId = new AtomicLong();
+public class SuricataFactProcessor extends AbstractRockNSMProcessor {
 
 	public void process(Exchange exchange) throws Exception {
-		Message inMessage = exchange.getIn();
-		String jsonString = inMessage.getBody(String.class);
-		JsonNode objTree = mapper.readTree(jsonString);
-
-		long id = uniqueId.incrementAndGet();
-		String hostname = objTree.at("/host/name").asText();
-		Date timestamp = Date.from(Instant.parse(objTree.at("/@timestamp").asText()));
-
-		inMessage.setHeader("JMSXGroupID", hostname);
-		inMessage.setHeader("JMSXGroupSeq", id);
-		inMessage.setBody(new SuricataFact(id, jsonString, hostname, timestamp));
+		doProcess(exchange, "/host/name", "/@timestamp", SuricataFact.class);
 	}
 }
