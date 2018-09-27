db1467814f440527d5cf7c1f06acad132e31dfa9
rlucente <rlucente@redhat.com>
2018-09-27 01:58:00 -0400

    use json objectmapper to convert to java

--- src/main/java/com/redhat/camel/BroFactProcessor.java
+++ src/main/java/com/redhat/camel/BroFactProcessor.java
@@ -1,11 +1,30 @@
 package com.redhat.camel;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import it.redhat.hacep.playground.rules.model.BroFact;
 
-public class BroFactProcessor extends AbstractRockNSMProcessor {
+public class BroFactProcessor implements Processor {
+	protected static final ObjectMapper mapper = new ObjectMapper();
+	protected static final AtomicLong uniqueId = new AtomicLong();
 
 	public void process(Exchange exchange) throws Exception {
-		doProcess(exchange, "/@system", "/ts", BroFact.class);
+		Message inMessage = exchange.getIn();
+		String jsonString = inMessage.getBody(String.class);
+
+		BroFact broFact = mapper.readValue(jsonString, BroFact.class);
+
+		long id = uniqueId.incrementAndGet();
+		broFact.setId(id);
+
+		inMessage.setHeader("JMSXGroupID", broFact.get_at_system());
+		inMessage.setHeader("JMSXGroupSeq", Long.toString(id));
+		inMessage.setBody(broFact);
 	}
 }
